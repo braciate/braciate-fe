bac658bd9b0dbb882d19b600914ef089b671e48d
Risqi Fahreal <farelrizqi377@gmail.com>
2024-08-21 13:02:35 +0700

    default

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import Nominasi from "./component/Nominasi";
-import Panduan from "./component/Panduan";
-import Rangkaian from "./component/Rangkaian";
-
 function App() {
-  return (
-    <div className="App">
-      <Rangkaian />
-      <Panduan />
-      <Nominasi />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
