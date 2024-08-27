46d12490ae19f8fc2546f62891f8307eb2103640
Risqi Fahreal <farelrizqi377@gmail.com>
2024-08-27 09:52:41 +0700

    change frm

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Panduan from "./component/Panduan";
 import Nominasi from "./component/Nominasi";
 import Rangkaian from "./component/Rangkaian";
 
-function App() {
+const App=()=> {
   return (
     <div className="App">
       <Rangkaian />
