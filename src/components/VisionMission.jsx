3532216c9d899a9e6c802e7161e7a719225e4d0b
Indah Githa <indahgitha99@gmail.com>
2024-09-02 08:15:13 +0700

    feat: vision and mission page

--- src/components/VisionMission.jsx
+++ src/components/VisionMission.jsx
@@ -9,7 +9,20 @@ const VisionMission = () => {
     return (
         <main className="vision-mission">
             <div className="background-icons">
-
+                <img src={iconLeft} alt="" id="icon-left"/>
+                <img src={iconRight} alt="" id="icon-right" />
+            </div>
+            <div className="vision-container">
+                <p id="vision-info">Bring The Great Story with Harmonization and Collaborative Simpul Brawijaya.</p>
+                <img src={visiText} alt="" id="visi-text" />
+            </div>
+            <div className="mission-container">
+                <img src={misiText} alt="" id="misi-text" />
+                <p>
+                    1. Menciptakan dan memaksimalkan kolaboratif simpul brawijaya yang berlandaskan kebersamaan dan pengembangan<br />
+                    2. Menciptakan branding serta citra positif bagi simpul brawijaya dengan semangat sinergi dan harmonisasi<br />
+                    3. Memotivasi simpul brawijaya menjadi wadah mahasiswa yang berimpact, berintegritas, dan future oriented
+                </p>
             </div>
         </main>
     );
