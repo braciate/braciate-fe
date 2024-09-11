b2570448513943597961a62673a56987259d03fa
Indah Githa <indahgitha99@gmail.com>
2024-09-11 15:41:22 +0700

    updated responsiveness in hero sect. and navbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import heroTitle from '../assets/img/herotitlenotrophy.svg';
 import pialaImg from '../assets/img/piala.png';
+import heroTitleAndro from '../assets/img/braciate-title-andro.svg';
 import './HeroSection.css';
 
 const HeroSection = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    const handleResize = () => {
+        setIsMobile(window.innerWidth < 768);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <main className='hero-section'>
             <div className='hero-title'>
                 <img id='piala-img' src={pialaImg} alt="" />
-                <img id='title-hero' src={heroTitle} alt="" />
+                <img id='title-hero' src={isMobile ? heroTitleAndro : heroTitle} alt="Hero Title" />
             </div>
         </main>
     );
