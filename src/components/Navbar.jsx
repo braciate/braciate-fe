d53e88522865fb72be12c839a43efc49dc0468a6
Indah Githa <indahgitha99@gmail.com>
2024-09-11 13:18:16 +0700

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         <li><a href="#home">HOME</a></li>
         <li><a href="#about">ABOUT US</a></li>
         <li><a href="#faq">FAQ</a></li>
-        <li  id='votenow'><a href="#vote">VOTE NOW</a></li>
+        <li id="votenow"><a href="#vote">VOTE NOW</a></li>
       </ul>
       <div className="navbar-toggle" onClick={toggleMenu}>
         <span className="bar"></span>
