923a6d6e4e29f7927d66889b3aa952369a0b6c7d
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-09-23 19:28:23 +0700

    fead: add ukm folder

---
+++ src/app/(ukm)/ukm/bestProductive/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { FC } from "react";
+
+const page: FC = () => {
+  return (
+    <div>
+      <h1>page best produktif</h1>
+    </div>
+  );
+};
+
+export default page;
