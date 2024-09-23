a9f3eba14b6e2e2481ab654d20a5e4d1f28c9513
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-09-23 18:13:01 +0700

    feat: edit login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@
 import BgAuth from "@/components/login/background/bg-auth";
 import type { FC } from "react";
 import useLoginForm from "@/hooks/useLoginForm";
-import { Mail, Lock } from "lucide-react";
+import { Lock, EyeIcon, EyeOffIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,16 +14,27 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import style from "@/components/login/style/auth.module.css";
 
 const LoginPage: FC = () => {
-  const { form, onSubmit } = useLoginForm();
+  const { form, onSubmit, showPassword, setShowPassword } = useLoginForm();
   const { errors, isSubmitted } = form.formState;
 
   return (
     <main className="flex h-screen justify-center relative">
       <BgAuth />
-      <section className="w-1/2 mx-5 md:mx-8 h-fit md:h-5/6 self-center grid">
-        <h2 className="font-jaoren text-4xl text-white text-center">Login</h2>
+      <section className="w-1/2 mx-5 md:mx-8 h-fit md:h-5/6 self-center grid space-y-4 z-10">
+        <div className="text-center space-y-0">
+          <h1
+            className="font-jaoren text-9xl text-transparent"
+            id={style.title_auth}
+          >
+            Login
+          </h1>
+          <h2 className="font-jaoren text-3xl tracking-wider">
+            Masuk Menggunakan Akun UB
+          </h2>
+        </div>
         <div className=" bg-black rounded-3xl">
           <div className="flex flex-col md:flex-row h-full">
             <div className="p-7 w-full my-auto">
@@ -33,7 +44,7 @@ const LoginPage: FC = () => {
                     e.preventDefault();
                     void form.handleSubmit(onSubmit)(e);
                   }}
-                  className="space-y-4"
+                  className="space-y-4 text-black"
                 >
                   {errors.root != null && (
                     <p className="text-sm bg-red-100 p-3 rounded-lg text-red-500 font-medium">
@@ -51,10 +62,9 @@ const LoginPage: FC = () => {
                             <Input
                               placeholder="Email address"
                               type="email"
-                              className={`py-6 focus:border-green-400 focus:border-2 focus:outline-none focus:ring-0 focus:ring-offset-0 transition-colors duration-200${Boolean(errors.email) && Boolean(isSubmitted) && " border-red-500"}`}
+                              className={`py-6 focus:border-green-400 focus:border-2 focus:outline-none focus:ring-0 focus:ring-offset-0 transition-colors duration-200 ${Boolean(errors.email) && Boolean(isSubmitted) && " border-red-500"}`}
                               {...field}
                             />
-                            <Mail className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
                           </div>
                         </FormControl>
                         <FormMessage className="text-white" />
@@ -70,23 +80,35 @@ const LoginPage: FC = () => {
                         <FormControl>
                           <div className="relative">
                             <Input
-                              type="password"
+                              type={showPassword ? "text" : "password"}
                               className={`py-6 focus:border-green-400 focus:border-2 focus:outline-none focus:ring-0 focus:ring-offset-0 transition-colors duration-200 ${Boolean(errors.password) && Boolean(isSubmitted) ? "border-red-500" : null} `}
                               placeholder="Password"
                               {...field}
                             />
-                            <Lock className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
+                            <button
+                              type="button"
+                              onClick={() => {
+                                setShowPassword(!showPassword);
+                              }}
+                              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400"
+                            >
+                              {showPassword ? (
+                                <EyeOffIcon className="h-5 w-5" />
+                              ) : (
+                                <EyeIcon className="h-5 w-5" />
+                              )}
+                            </button>
                           </div>
                         </FormControl>
                         <FormMessage className="text-white" />
                       </FormItem>
                     )}
                   />
-
                   <div className="pt-6">
                     <Button
                       type="submit"
-                      className="w-full py-6 bg-green-500 hover:bg-green-600 text-white"
+                      className="w-full py-6 hover:brightness-75 text-black font-jaoren text-3xl border-white border-2"
+                      id={style.submitButton}
                     >
                       Masuk
                     </Button>
--- src/components/login/background/bg-auth.tsx
+++ src/components/login/background/bg-auth.tsx
@@ -1,14 +1,21 @@
 import type { FC } from "react";
 import Image from "next/image";
 import BgLogin from "@/assets/svg/background/bg-auth.svg";
+import glitter from "@/assets/img/background/glitter.png";
 
 const BgAuth: FC = () => {
   return (
-    <div className="fixed w-screen h-screen -z-10 brightness-75">
+    <div className="absolute w-full h-full brightness-75">
+      <Image
+        src={glitter}
+        alt="glitter"
+        fill={true}
+        className="object-cover z-0"
+      />
       <Image
         src={BgLogin}
         alt="bg auth"
-        className="object-cover bg-repeat h-screen"
+        className="object-cover -z-10 brightness-75"
         fill={true}
       />
     </div>
