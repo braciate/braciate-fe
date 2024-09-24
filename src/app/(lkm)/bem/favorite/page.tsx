a500df4a5a7ab3b19f3871dcf46ec78e1a0bc3b4
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-09-24 22:14:34 +0700

    config: add tempalte paging categories

---
+++ src/app/(lkm)/bem/favorite/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { FC } from "react";
+
+const Page: FC = () => {
+  return (
+    <div>
+      <h1>page favorite</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/lkm/background/bg-main.tsx
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+import Image from "next/image";
+import Bg from "../../../assets/svg/background/bg-kipas.svg";
+
+const BgKategori: FC = () => {};
