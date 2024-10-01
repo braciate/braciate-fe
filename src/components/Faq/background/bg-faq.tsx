358d69a6330adf1358f7ac4d5ab2d3743e131d58
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-10-01 19:06:57 +0700

    style: adjust faq fade

--- src/components/Faq/background/bg-faq.tsx
+++ src/components/Faq/background/bg-faq.tsx
@@ -1,7 +1,7 @@
 import type { FC } from "react";
 import Image from "next/image";
 import BgFaqSection from "@/assets/svg/background/bg-faq.svg";
-
+import ShadowBot from "@/assets/img/fragments/blackshadow-bottom.png";
 const BgFaq: FC = () => {
   return (
     <div className="absolute w-full h-full brightness-50 -z-10 bg-black">
@@ -10,6 +10,11 @@ const BgFaq: FC = () => {
         alt="bg auth"
         className="object-cover h-screen"
         fill={true}
+      />
+      <Image
+        src={ShadowBot}
+        alt="bg auth"
+        className="absolute h-full bottom-0"
       />
     </div>
   );
