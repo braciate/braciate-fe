e8c38562e4baeb2cfd9b15224b2b02c7cda351db
Risqi Fahreal <106820529+Xfhreall@users.noreply.github.com>
2024-10-20 17:34:18 +0700

    fix: responsive

--- src/app/(credit)/credit/page.tsx
+++ src/app/(credit)/credit/page.tsx
@@ -84,14 +84,14 @@ const teamMembers: TeamMember[] = [
 
 const TeamMemberCard: FC<TeamMember> = ({ name, linkedin, instagram }) => (
   <div className="flex gap-2 sm:gap-6 my-4">
-    <div className="border-2 p-0.5 sm:p-4 rounded-full bg-white text-center w-full flex gap-2 justify-center text-black text-lg sm:text-xl md:text-2xl items-center">
+    <div className="border-2 p-0.5 sm:p-4 rounded-full bg-white text-center w-full flex gap-2 justify-center text-black text-sm sm:text-xl md:text-2xl items-center">
       <h2>{name}</h2>
     </div>
     <div className="flex items-center gap-2 sm:gap-4">
       <Link
         href={instagram}
         target="_blank"
-        className={`flex items-center justify-center rounded-full bg-white p-5 text-black w-12 h-12 sm:w-16 sm:h-16 ${styles.footer} fill-black hover:fill-white`}
+        className={`flex items-center justify-center rounded-full bg-white p-3 md:p-5 text-black w-12 h-12 sm:w-16 sm:h-16 ${styles.footer} fill-black hover:fill-white`}
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
           <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
@@ -111,7 +111,7 @@ const TeamMemberCard: FC<TeamMember> = ({ name, linkedin, instagram }) => (
 const TeamSection: FC<{ title: string; role: string }> = ({ title, role }) => (
   <section className="mt-10">
     <h2
-      className={`text-3xl sm:text-4xl md:text-5xl font-bold mb-4 p-2 sm:p-4 text-white rounded-full font-jaoren w-5/12 ${text.subtitle}`}
+      className={`text-3xl sm:text-4xl md:text-5xl font-bold mb-4 p-2 sm:p-4 text-white font-jaoren w-full ${text.subtitle}`}
     >
       {title}
     </h2>
